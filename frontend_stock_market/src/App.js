f2d1f76e2c6e4c0ed2af47c8c0fd818ee6fb2920
Sanchit Patil <Sanchit.Patil@wissen.com>
2025-03-20 12:49:37 +0530

    Routes

--- frontend_stock_market/src/App.js
+++ frontend_stock_market/src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Footer from './Components/Footer';
 import Header from './Components/Header';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Register from './Screens/Register';
+import Login from './Screens/Login';
 
 function App() {
   return (
@@ -10,10 +12,10 @@ function App() {
 
       <div class="content">
         <Router>
-
           <div class="main-content">
             <Routes>
-              
+              <Route path="/register" element={<Register />} />
+              <Route path="/login" element={<Login />} />
             </Routes>
           </div>
         </Router>
--- frontend_stock_market/src/Components/Header.js
+++ frontend_stock_market/src/Components/Header.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import './Header.css';
+import { Link } from 'react-router-dom';
 
 function Header() {
     return (
@@ -10,10 +11,15 @@ function Header() {
             </div>
             <nav>
                 <ul>
-                    <li><a >WatchList</a></li>
+                    <li><a>WatchList</a></li>
                     <li><a >Holding</a></li>
                     <li><a >Balance</a></li>
                     <li><a >Register</a></li>
+
+                    {/* <li><Link to="/watchlist">WatchList</Link></li>
+                    <li><Link to="/holding">Holding</Link></li>
+                    <li><Link to="/balance">Balance</Link></li>
+                    <li><Link to="/register">Register</Link></li> */}
                 </ul>
             </nav>
         </header>
