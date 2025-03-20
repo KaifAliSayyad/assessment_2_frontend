95c23ba6443e5d4127a66ba5d52ac44b52cd5aa6
Sanchit Patil <Sanchit.Patil@wissen.com>
2025-03-20 12:16:40 +0530

    Pushed updated headers and footers

--- frontend_stock_market/src/Components/Footer.js
+++ frontend_stock_market/src/Components/Footer.js
@@ -5,9 +5,9 @@ function Footer() {
     return (
         <footer>
             <div class="footer-content">
-                <p>&copy; 2025 Our Bank. All rights reserved.</p>
+                <p>&copy; 2025 Stock Market. All rights reserved.</p>
                 <div class="social-links">
-                    <a href="/" target="_blank" class="social-icon">www.ourbank.com</a>
+                    <a href="/" target="_blank" class="social-icon">www.stockmarket.com</a>
                 </div>
             </div>
         </footer>
